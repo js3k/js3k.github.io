5d981bb3ac0469880c08a3891d2b5d80895bad5a
js3k <93347756+js3k@users.noreply.github.com>
2025-10-01 00:28:11 +0700

    Add files via upload

--- wasm/dots.c
+++ wasm/dots.c
@@ -1,52 +1,53 @@
+#include <emscripten.h>
 #include <stdlib.h>
-#include <time.h>
 
-// Definisi struktur Dot
-typedef struct {
-    float x, y;
-    float vx, vy;
-} Dot;
+#define MAX_DOTS 10000
 
-// Pointer untuk array dinamis
-Dot* dots = NULL;
-int num_dots = 0;
+float x[MAX_DOTS];
+float y[MAX_DOTS];
+float vx[MAX_DOTS];
+float vy[MAX_DOTS];
+int count = 100;
+float speed = 1.0;
 
-void resize_and_reinit_dots(int new_num_dots) {
-    if (new_num_dots == num_dots) return;
-
-    // Alokasikan ulang memori
-    dots = (Dot*)realloc(dots, new_num_dots * sizeof(Dot));
-    if (dots == NULL) {
-        // Penanganan kesalahan jika realloc gagal
-        return;
-    }
-
-    // Inisialisasi titik-titik baru jika jumlahnya bertambah
-    if (new_num_dots > num_dots) {
-        for (int i = num_dots; i < new_num_dots; i++) {
-            dots[i].x = rand() % 350;
-            dots[i].y = rand() % 600;
-            dots[i].vx = ((rand() % 100) - 50) / 50.0f;
-            dots[i].vy = ((rand() % 100) - 50) / 50.0f;
-        }
-    }
-    num_dots = new_num_dots;
+EMSCRIPTEN_KEEPALIVE
+void setCount(int c) {
+  if (c > MAX_DOTS) c = MAX_DOTS;
+  count = c;
+  for (int i = 0; i < count; i++) {
+    x[i] = rand() % 350;
+    y[i] = rand() % 600;
+    vx[i] = (rand() % 100 - 50) / 50.0;
+    vy[i] = (rand() % 100 - 50) / 50.0;
+  }
 }
 
-void update_dots() {
-    for (int i = 0; i < num_dots; i++) {
-        dots[i].x += dots[i].vx;
-        dots[i].y += dots[i].vy;
-
-        if (dots[i].x < 0 || dots[i].x > 350) dots[i].vx *= -1;
-        if (dots[i].y < 0 || dots[i].y > 600) dots[i].vy *= -1;
-    }
+EMSCRIPTEN_KEEPALIVE
+void setSpeed(float s) {
+  speed = s;
 }
 
-Dot* get_dots() {
-    return dots;
+EMSCRIPTEN_KEEPALIVE
+float getX(int i) {
+  return x[i];
 }
 
-int get_num_dots() {
-    return num_dots;
+EMSCRIPTEN_KEEPALIVE
+float getY(int i) {
+  return y[i];
+}
+
+EMSCRIPTEN_KEEPALIVE
+int getCount() {
+  return count;
+}
+
+EMSCRIPTEN_KEEPALIVE
+void update() {
+  for (int i = 0; i < count; i++) {
+    x[i] += vx[i] * speed;
+    y[i] += vy[i] * speed;
+    if (x[i] < 0 || x[i] > 350) vx[i] = -vx[i];
+    if (y[i] < 0 || y[i] > 600) vy[i] = -vy[i];
+  }
 }
